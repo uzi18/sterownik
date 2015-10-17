32b35fb009dfa959814973a4b86bf1c826557549
Bartlomiej Zimon <uzi18@o2.pl>
2015-10-17 19:39:30 +0200

    - literka

--- retortowy-P.py
+++ retortowy-P.py
@@ -53,7 +53,7 @@ while (c.getStatus()):
     elif (delta_poprzednia > 0):
       c.setRetRecznyDmuchawa(start_dmuchawa)
       c.setRetRecznyPostoj(start_postoj)
-      c.setRetRecznyPodawani(start_podawanie)
+      c.setRetRecznyPodawanie(start_podawanie)
       print("POWROT Delta:"+ str(delta)+" dmuchanie:" + str(start_dmuchawa) + " podawanie:" + str(start_podawanie) + " postoj:" + str(start_postoj))
     else:
       print("Delta:"+ str(delta)+" Poprzednia:" + str(delta_poprzednia))
