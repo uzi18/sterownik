689f6cddea256ad00626f24402cbbcad2ec88328
Bartlomiej Zimon <uzi18@o2.pl>
2015-10-17 18:19:53 +0200

    - drobne zmiany i dodanie informacji na konsole ...

--- retortowy-P.py
+++ retortowy-P.py
@@ -17,12 +17,12 @@ password = 'admin'
 zadana_co = 65
 
 korekcja_podawania = 1.0
-korekcja_postoju = 1.0
+korekcja_postoju =  -1.0
 korekcja_dmuchania = 1.0
 
 start_podawanie = 15
 start_postoj = 35
-start_dmuachawa = 43
+start_dmuchawa = 43
 
 czas_cyklu = 60
 
@@ -36,15 +36,22 @@ poprzednia_co = c.getTempCO()
 
 while (c.getStatus()):
   if (c.getTrybAuto() && c.getTypKotla() == "RETORTOWY-RECZNY"):
+    delta = int(zadana_co - c.getTempCO() +0.5)
+    delta_poprzednia = int(poprzednia_co - c.getTempCO() +0.5)
+    
     if (c.getTempCO() < zadana_co - 1):
-      delta = int(zadana_co - c.getTempCO() +0.5)
-      c.setRetRecznyDmuchawa( delta * korekcja_dmuchania + start_dmuachawa)
-      c.setRetRecznyPostoj(   delta * korekcja_postoju   + start_postoj)
-      c.setRetRecznyPodawanie(delta * korekcja_podawania + start_podawanie)
-    else:
-      c.setRetRecznyDmuchawa(start_dmuachawa)
+      nowe_podawanie = delta * korekcja_podawania + start_podawanie
+      nowe_postoj    = delta * korekcja_postoju   + start_postoj
+      nowe_dmuchanie = delta * korekcja_dmuchania + start_dmuchawa
+      c.setRetRecznyDmuchawa(nowe_dmuchanie)
+      c.setRetRecznyPostoj(nowe_postoj)
+      c.setRetRecznyPodawanie(nowe_podawanie)
+      print("NOWE   Delta:"+ str(delta)+" dmuchanie:" + str(nowe_dmuchanie) + " podawanie:" + str(nowe_podawanie) + " postoj:" + str(nowe_postoj))
+    elif (delta_poprzednia > 0):
+      c.setRetRecznyDmuchawa(start_dmuchawa)
       c.setRetRecznyPostoj(start_postoj)
       c.setRetRecznyPodawani(start_podawanie)
+      print("POWROT Delta:"+ str(delta)+" dmuchanie:" + str(start_dmuchawa) + " podawanie:" + str(start_podawanie) + " postoj:" + str(start_postoj))
       
   poprzednia_co = c.getTempCO()
   time.sleep(czas_cyklu)
