cd55bce086251317d91248f24b6301be74d41de6
Bartlomiej Zimon <uzi18@o2.pl>
2015-10-17 21:32:21 +0200

    - zapamietywanie poprzednich nastaw aby nie wysylac nowych danych jesli nie trzeba ...

--- retortowy-P.py
+++ retortowy-P.py
@@ -40,6 +40,9 @@ c.setRetRecznyDmuchawa(rozped_dmuchawa)
 c.setRetRecznyPostoj(rozped_postoj)
 c.setRetRecznyPodawanie(rozped_podawanie)
 poprzednia_co = c.getTempCO()
+poprzednie_dmuchanie = nowe_dmuchanie = rozped_dmuchawa
+poprzednie_postoj = nowe_postoj = rozped_postoj
+poprzednie_podawanie = nowe_podawanie = rozped_podawanie
 
 while (c.getStatus()):
   if (c.getTrybAuto() and c.getTypKotla() == "RETORTOWY-RECZNY"):
@@ -53,17 +56,26 @@ while (c.getStatus()):
       if (nowe_podawanie <= 0): nowe_podawanie = 1
       if (nowe_postoj    <= 0): nowe_postoj = 1
       if (nowe_dmuchanie <=25): nowe_dmuchanie = 25
-      c.setRetRecznyDmuchawa(nowe_dmuchanie)
-      c.setRetRecznyPostoj(nowe_postoj)
-      c.setRetRecznyPodawanie(nowe_podawanie)
       print("NOWE   Delta:"+ str(delta)+" dmuchanie:" + str(nowe_dmuchanie) + " podawanie:" + str(nowe_podawanie) + " postoj:" + str(nowe_postoj))
-    elif (delta_poprzednia >= 0):
-      c.setRetRecznyDmuchawa(rozped_dmuchawa)
-      c.setRetRecznyPostoj(rozped_postoj)
-      c.setRetRecznyPodawanie(rozped_podawanie)
-      print("POWROT Delta:"+ str(delta)+" dmuchanie:" + str(rozped_dmuchawa) + " podawanie:" + str(rozped_podawanie) + " postoj:" + str(rozped_postoj))
+    elif (delta_poprzednia >= 0 and delta <= 0):
+      nowe_dmuchanie = rozped_dmuchawa
+      nowe_postoj = rozped_postoj
+      nowe_podawanie =rozped_podawanie
+      print("ROZPED Delta:"+ str(delta)+" dmuchanie:" + str(rozped_dmuchawa) + " podawanie:" + str(rozped_podawanie) + " postoj:" + str(rozped_postoj))
     else:
       print("Delta:"+ str(delta)+" Poprzednia:" + str(delta_poprzednia))
-      
+
+  if (nowe_dmuchanie <> poprzednie_dmuchanie):
+    c.setRetRecznyDmuchawa(nowe_dmuchanie)
+    poprzednie_dmuchanie = nowe_dmuchanie
+
+  if (nowe_postoj <> poprzednie_postoj):
+    c.setRetRecznyPostoj(nowe_postoj)
+    poprzednie_postoj = nowe_postoj
+
+  if (nowe_podawanie <> poprzednie_podawanie):
+    c.setRetRecznyPodawanie(nowe_podawanie)
+    poprzednie_podawanie = nowe_podawanie
+
   poprzednia_co = c.getTempCO()
   time.sleep(czas_cyklu)
