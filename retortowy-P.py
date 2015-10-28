dc8bb9398df316cc14e562b2d85ea8b7eb55759a
Bartlomiej Zimon <uzi18@o2.pl>
2015-10-28 20:39:29 +0100

    - moc w jedno miejsce ...

--- retortowy-P.py
+++ retortowy-P.py
@@ -86,14 +86,13 @@ while (c.getStatus()):
       nowe_postoj    = delta * korekcja_postoju   + start_postoj
       nowe_dmuchanie = delta * korekcja_dmuchania + start_dmuchawa
       if (nowe_podawanie == 0): nowe_podawanie = 1
+      nowe_moc = float(nowe_postoj)/float(nowe_podawanie)
       if (nowe_podawanie < pod_min):
-        moc = float(nowe_postoj)/float(nowe_podawanie)
         nowe_podawanie = pod_min
-        nowe_postoj = int(moc*pod_min)
+        nowe_postoj = int(nowe_moc*pod_min)
       if (nowe_podawanie > pod_max):
-        moc = float(nowe_postoj)/float(nowe_podawanie)
         nowe_podawanie = pod_max
-        nowe_postoj = int(moc*pod_max)
+        nowe_postoj = int(nowe_moc*pod_max)
       if (nowe_postoj    < pod_min): nowe_postoj = pos_min
       if (nowe_postoj    > pod_max): nowe_postoj = pos_max
       if (nowe_dmuchanie < dmu_min): nowe_dmuchanie = dmu_min
