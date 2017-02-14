8f61b65df15b63822431714407dd48394549a6fb
Bartlomiej Zimon <uzi18@o2.pl>
2017-02-14 02:53:01 +0100

    - Domoticz: dodanie obslugi polaczenia przez port szeregowy

--- arduino/Domoticz/konfiguracja.py
+++ arduino/Domoticz/konfiguracja.py
@@ -1,5 +1,8 @@
 
+# polaczenie z ethernet shield:
 ip_lucjan = '192.168.2.4'
+# lub polaczenie przez port szeregory:
+#ip_lucjan = '/dev/ttyACM0'
 ip_domoticz = 'localhost'
 port_domoticz = 8080
 idx_start = 1
--- arduino/Domoticz/test.py
+++ arduino/Domoticz/test.py
@@ -11,6 +11,8 @@ else:
 import time 
 import string
 
+rs = None
+
 try:
   import konfiguracja
 except ImportError:
@@ -36,25 +38,46 @@ if not 'interwal' in dir(konfiguracja):
   
 print(" interwal = "+str(konfiguracja.interwal)+"s.")
 
+if konfiguracja.ip_lucjan.count('.') == 0:
+  import serial
+  rs=serial.Serial(konfiguracja.ip_lucjan,115200)
+  time.sleep(20)
+  rs.flushInput()
+  rs.flushOutput()
+
 lucek = "http://"+konfiguracja.ip_lucjan+"/t.json"
 domoticz = "http://"+konfiguracja.ip_domoticz+":"+str(konfiguracja.port_domoticz)+"/json.htm?type=command&param=udevice&idx="
 value = "&nvalue=0&svalue="
 
 while 1:
   try:
-    response = urlopen(lucek)
-    data = response.read().decode("utf-8")
-    data = data.replace('},{"t": ', ',')
-    data = data.replace('{"thermos":[{"t": ', '')
-    data = data.split("}],", 1)[0]
-    data = data.split(",")
-    
+    data = ''
+    if rs == None:
+      response = urlopen(lucek)
+      data = response.read().decode("utf-8")
+      data = data.replace('},{"t": ', ',')
+      data = data.replace('{"thermos":[{"t": ', '')
+      data = data.split("}],", 1)[0]
+      data = data.split(",")
+    else:
+      rs.flushInput()
+      rs.flushOutput()
+      rs.write('t')
+      while not (data.startswith("t:[") and data.endswith("\r\n")):
+        data = rs.readline()
+
+      data = data.replace('t:[', '')
+      data = data.replace(']\r\n', '')
+      data = data.split(",")
+      data = [float(i)/10 for i in data]
+      
     print(data)
     
     idx = konfiguracja.idx_start
-    for x in range(16):
-      t = data[x]
-      response = urlopen(domoticz + str(idx+x) + value + t)
+    if len(data) == 16:
+      for x in range(16):
+        t = data[x]
+        response = urlopen(domoticz + str(idx+x) + value + str(t))
 
   except:
     pass
